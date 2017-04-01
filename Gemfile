8b69c05b6a5fb38d423ab9483abd828d83d62e8b
Lee McDowell <shadolee@mail.com>
2017-04-01 22:32:06 +1100

    add rolify and pundit gems

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem "pundit"
+
+gem "rolify"
+
 gem 'simple_form'
 gem 'friendly_id', '~> 5.1.0'
 gem "bulma-rails", "~> 0.4.0"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook] 
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :posts
   has_many :comments
+
+  def admin?
+    has_role?(:admin)
+  end 
 end
